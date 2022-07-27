b001e1a2ab069c7d01d748236bd0cda96210059f
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-07-27 10:09:05 +1000

    trying to get darts to work properly. everything is running on cpu now, and I'm getting endless logs which is annoying and slow. I think this is the hardest my CPU has had to work. It sits at 100% while running one of these models.

---
+++ data_sources.py
@@ -0,0 +1,88 @@
+import os
+
+data = 'data'
+
+# energy & weather
+energy = os.path.join(data, 'energy & weather', 'energy_dataset.csv')
+weather = os.path.join(data, 'energy & weather', 'weather_features.csv')
+
+# greenhouse_dataset
+GreenhouseClimate1 = os.path.join(data, 'greenhouse_dataset', 'GreenhouseClimate1')
+
+# m5_forecasting_accuracy
+sales_train_validation = os.path.join(data, 'm5_forecasting_accuracy', 'sales_train_validation.csv')
+sales_train_evaluation = os.path.join(data, 'm5_forecasting_accuracy', 'sell_prices.csv')
+calendar = os.path.join(data, 'm5_forecasting_accuracy', 'calendar.csv')
+
+# nifty_50_stock_market_data_2000_2020
+stock_name = 'TATASTEEL'
+stocks = os.path.join(data, 'nifty_50_stock_market_data_2000_2020', f'{stock_name}.csv')
+
+# norway_bicycle
+bikerides_day = os.path.join(data, 'norway_bicycle', 'bikerides_day.csv')
+
+# partial visuelle
+partial_visuelle_train = os.path.join(data, 'partial_visuelle', 'partial_visuelle_train.csv')
+partial_visuelle_test = os.path.join(data, 'partial_visuelle', 'partial_visuelle_test.csv')
+# telco customer churn
+
+# ts_data_1
+ambient_temperature_system_failure = os.path.join(data, 'ts_data_1', 'ambient_temperature_system_failure.csv')
+arma_series1 = os.path.join(data, 'ts_data_1', 'arma_series1.csv')
+arma_series2 = os.path.join(data, 'ts_data_1', 'arma_series2.csv')
+arma_series3 = os.path.join(data, 'ts_data_1', 'arma_series3.csv')
+avocado = os.path.join(data, 'ts_data_1', 'avocado.csv')
+daily_minimum_temperatures_in_me = os.path.join(data, 'ts_data_1', 'daily-minimum-temperatures-in-me.csv')
+dataset_temperature = os.path.join(data, 'ts_data_1', 'dataset_temperature.csv')
+diabetic = os.path.join(data, 'ts_data_1', 'diabetic.csv')
+energy_daily = os.path.join(data, 'ts_data_1', 'energy_daily.csv')
+ex1_wiki_visits = os.path.join(data, 'ts_data_1', 'ex1_wiki_visits.csv')
+example_air_passengers = os.path.join(data, 'ts_data_1', 'example_air_passengers.csv')
+example_retail_sales = os.path.join(data, 'ts_data_1', 'example_retail_sales.csv')
+example_wp_log_peyton_manning = os.path.join(data, 'ts_data_1', 'example_wp_log_peyton_manning.csv')
+example_wp_log_R_outliers1 = os.path.join(data, 'ts_data_1', 'example_wp_log_R_outliers1.csv')
+example_wp_log_R_outliers2 = os.path.join(data, 'ts_data_1', 'example_wp_log_R_outliers2.csv')
+exp1 = os.path.join(data, 'ts_data_1', 'exp1.csv')
+exp2 = os.path.join(data, 'ts_data_1', 'exp2.csv')
+exp3 = os.path.join(data, 'ts_data_1', 'exp3.csv')
+Fremont_Bridge = os.path.join(data, 'ts_data_1', 'Fremont_Bridge.csv')
+google_chg = os.path.join(data, 'ts_data_1', 'google_chg.csv')
+irish_electricity1 = os.path.join(data, 'ts_data_1', 'irish_electricity.csv')
+irish_electricity2 = os.path.join(data, 'ts_data_1', 'irish_electricity.xlsx')
+medium_posts = os.path.join(data, 'ts_data_1', 'medium_posts.csv')
+outdoor_temperature_UTC = os.path.join(data, 'ts_data_1', 'outdoor-temperature-12UTC.csv')
+passengers = os.path.join(data, 'ts_data_1', 'passengers.csv')
+passengers_test = os.path.join(data, 'ts_data_1', 'passengers_test.csv')
+passengers_train = os.path.join(data, 'ts_data_1', 'passengers_train.csv')
+quarterly_gdp_change = os.path.join(data, 'ts_data_1', 'quarterly_gdp_change.csv')
+rossman_sales = os.path.join(data, 'ts_data_1', 'rossman_sales.csv')
+Sample_Superstore = os.path.join(data, 'ts_data_1', 'Sample - Superstore.xls')
+savings_change = os.path.join(data, 'ts_data_1', 'savings_change.csv')
+sunspots = os.path.join(data, 'ts_data_1', 'sunspots.csv')
+tesla_prices_5y = os.path.join(data, 'ts_data_1', 'tesla_prices_5y.csv')
+us_covid = os.path.join(data, 'ts_data_1', 'us_covid.csv')
+us_energy = os.path.join(data, 'ts_data_1', 'us_energy.csv')
+wiki_log_R = os.path.join(data, 'ts_data_1', 'wiki_log_R.csv')
+
+# hourly energy consumption
+pjm_hourly_est = os.path.join(data, 'hourly_energy_consumption', 'pjm_hourly_est.csv')
+
+# US energy consumption
+Region_CAL = os.path.join(data, 'us_energy_consumption', 'Region_CAL.xlsx')
+
+# ventilator pressure prediction
+
+# WHO cases
+WHO_full_data2003 = os.path.join(data, 'who_cases', 'WHO_full_data2003.csv')
+
+# april 2020 tabular playground
+train = os.path.join(data, 'tps_april_2022', 'tps_apr22_train.csv')
+train_labels = os.path.join(data, 'tps_april_2022', 'tps_apr22_train_labels.csv')
+test = os.path.join(data, 'tps_april_2022', 'tps_apr22_test.csv')
+submission = os.path.join(data, 'tps_april_2022', 'sample_submission.csv')
+
+# demand forecasting kernels only
+demand_forecasting_training = os.path.join(data, 'demand_forecasting_kernals_only', 'demand_forecasting_train.csv')
+demand_forecasting_test = os.path.join(data, 'demand_forecasting_kernals_only' ,'demand_forecasting_test.csv')
+
+# claim number: 1181229051
